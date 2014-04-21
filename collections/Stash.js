c3e3ba683e79731bb43121079baea861c2777b26
Joseph Jung <jungyue@gmail.com>
2014-04-20 21:34:19 -0500

    changes to look and feel

---
+++ collections/Stash.js
@@ -0,0 +1,13 @@
+Stash = new Meteor.Collection('stash');
+
+Stash.allow({
+	insert : function(userId, doc) {
+		return true;
+	},
+	remove : function(userId, doc) {
+		return true;
+	},
+	update : function(userId, doc) {
+		return true;
+	}
+});
