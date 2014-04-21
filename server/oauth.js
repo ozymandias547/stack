c2a75eabb3a0367aed3ef5774e8c9ed218621135
joshjung <joshua.p.jung@gmail.com>
2014-04-20 20:22:21 -0500

    Merge + additiong of google OAUTH info.

--- server/oauth.js
+++ server/oauth.js
@@ -1,10 +1,11 @@
-
 Meteor.startup(function() {
-  
-  Accounts.loginServiceConfiguration.upsert({ service: 'google' }, {
-    service:  'google',
-    clientId: '1068648882472.apps.googleusercontent.com',
-    secret:   '19ZpPbrvzKyg1gzJLSR2079O'
-  });
+
+    Accounts.loginServiceConfiguration.upsert({
+        service: 'google'
+    }, {
+        service: 'google',
+        clientId: '643214640641-mb3urmmre5d06n2k02fjulct70e5a6q9.apps.googleusercontent.com',
+        secret: 'AlMkP_BH4SQ2ju4ItpIKba2c'
+    });
 
 });
