9e05384c132e183164e9f2105530c5a17ead3fe2
joshjung <joshua.p.jung@gmail.com>
2014-04-20 19:58:04 -0500

    Updates to clean up some things

--- client/router.js
+++ client/router.js
@@ -1,9 +1,9 @@
-
 Router.configure({
     layoutTemplate: 'stack'
 });
 
 Router.map(function() {
-    this.route('home', { path: '/' });
-    this.route('about'), { path: '/about'}
+    this.route('home', {
+        path: '/'
+    });
 });
--- client/views/home.js
+++ client/views/home.js
@@ -1,48 +1,51 @@
 // controller for home.html
 document.title = "Stack Home";
 
+var NULL = "null";
+
 Meteor.subscribe("stack");
 
-Session.set("selected", "none");
+Session.set("selectedID", NULL);
 
 Template.home.helpers({
-	stacks : function() {
-		return Stack.find().fetch();
-	},
-	tasks : function() {
-		
-		var selected = Session.get("selected");
-
-		if (selected!=="none"){
-			console.log(Stack.findOne({ _id : selected }).tasks);
-			return Stack.findOne({ _id : selected }).tasks;
-		}
-		else return [];
-	}
-})
+    stacks: function() {
+        return Stack.find().fetch();
+    },
+    tasks: function() {
+        return Session.get("selectedID") !== NULL ? Stack.findOne({
+            _id: Session.get("selectedID")
+        }).tasks : [];
+    }
+});
 
 Template.home.events({
-	"keydown .addStack" : function(event) {
-		console.log(event);
-		if (event.keyCode==13 && event.target.value !== "") {
-			Stack.insert({ name: event.target.value});
-			event.target.value = "";
-		}
-	},
-	"click .deleteStack" : function(event) {
-		var confirmed = confirm("Are you sure?");
-		if (confirmed) Stack.remove(this._id);
-	},
-	"click .stack" : function(event) {
-		$(".stack.selected").each(function() { $(this).removeClass("selected") });
-		$(event.currentTarget).addClass("selected");
-		Session.set("selected", this._id);
-	},
-	"click .addTask" : function(event) {
-		var selected = Session.get("selected");
-
-		if (selected!=="none") {
-			Stack.update(selected, { $push: { tasks: "hi" } })
-		}
-	}
+    "keydown .addStack": function(event) {
+        if (event.keyCode == 13 && event.target.value !== "") {
+            Stack.insert({
+                name: event.target.value
+            });
+            event.target.value = "";
+        }
+    },
+    "click .deleteStack": function(event) {
+        if (confirm("Are you sure?")) {
+            Stack.remove(this._id);
+        }
+    },
+    "click .stack": function(event) {
+        $(".stack.selected").each(function() {
+            $(this).removeClass("selected")
+        });
+        $(event.currentTarget).addClass("selected");
+        Session.set("selectedID", this._id);
+    },
+    "click .addTask": function(event) {
+        if (Session.get("selectedID") !== NULL) {
+            Stack.update(selected, {
+                $push: {
+                    tasks: "hi"
+                }
+            });
+        }
+    }
 });
