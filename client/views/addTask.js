75b4c4277970e97478c308439a150fc90b90f48a
Joseph Jung <jungyue@gmail.com>
2014-04-29 22:13:03 -0500

    Add placeholders and cleanup addTask helpers

--- client/views/addTask.js
+++ client/views/addTask.js
@@ -1,25 +1,25 @@
 Template.addTask.events({
+    
     "click .AddTask": function(event, template) {
-
-        console.log(template);
 
         var $addTaskButton = $(template.find(".AddTask"));
         var $addTaskInput = $(template.find(".AddTaskInput"));
 
         $addTaskButton.addClass("hidden");
         $addTaskInput.removeClass("hidden");
+        
         $addTaskInput.focus();
         
     },
+
     "keydown .AddTaskInput": function(event, template) {
         
-        console.log(this);
-
         var $addTaskButton = $(template.find(".AddTask"));
         var $addTaskInput = $(template.find(".AddTaskInput"));
         
         if (event.keyCode == 13) {
-            if ($addTaskInput.value != '') {
+            if ($addTaskInput.val() != '') {
+                
                 Task.insert({
                     userId: Meteor.userId(),
                     stackId: this._id,
@@ -27,10 +27,11 @@ Template.addTask.events({
                     priority: 0
                 });
             }
-            $addTaskInput.value = '';
+            $addTaskInput.val("");
             
             $addTaskButton.removeClass("hidden");
             $addTaskInput.addClass("hidden");
         }
     }
+
 })
