ad8f6a1ca798e36a46e226d8ce771a736350f419
Joseph Jung <jungyue@gmail.com>
2014-05-02 22:58:37 -0500

    Add priority logic for sorting

--- client/views/task/tasks.js
+++ client/views/task/tasks.js
@@ -4,18 +4,51 @@ Template.tasks.helpers({
             stackId: this._id
         }, {
             sort: {
-                priority: -1
+                priority: 1
             }
         });
     }
 });
+
+SimpleRationalRanks = {
+  beforeFirst: function (firstRank) { return firstRank - 1; },
+  between: function (beforeRank, afterRank) { return (beforeRank + afterRank) / 2; },
+  afterLast: function (lastRank) { return lastRank + 1; }
+};
+
 Template.tasks.rendered = function() {
     $( ".sortable" ).each(function() {
         $(this).sortable({
             placeholder: "ui-state-highlight",
-            connectWith: ".taskSortableShare",
-            handle: ".handle"
+            // connectWith: ".taskSortableShare",  
+            handle: ".handle",
+            stop: function(event, ui) {
+                
+                var el = ui.item.get(0),
+                    before = ui.item.prev().get(0),
+                    after = ui.item.next().get(0);
+
+                var newRank;
+
+                // Moving to the top of the list
+                if(!before) {
+                    console.log("first in rank");
+                    newRank = SimpleRationalRanks.beforeFirst( UI.getElementData(after).priority );
+                } else if(!after) {
+                    newRank = SimpleRationalRanks.afterLast( UI.getElementData(before).priority )
+                } else {
+                    newRank = SimpleRationalRanks.between(
+                        UI.getElementData(before).priority,
+                        UI.getElementData(after).priority
+                    );
+                }
+
+                Task.update(UI.getElementData(el)._id, { $set: { priority: newRank }})
+
+            }
+
         });
+
         $(this).disableSelection();
     });
 }
