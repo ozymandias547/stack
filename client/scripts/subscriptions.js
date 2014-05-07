17da4215c932a9b0df20e088d748160ab5de82dc
joshjung <joshua.p.jung@gmail.com>
2014-05-06 23:57:21 -0500

    Updates to styling, primarily

--- client/scripts/subscriptions.js
+++ client/scripts/subscriptions.js
@@ -8,6 +8,8 @@ Deps.autorun(function() {
     Meteor.subscribe("userData", null, function() {
         Session.set("userData", Meteor.user());
 
+        // Once user data is retrieved, we can then request
+        // the all the facebook friends of that user.
         Meteor.call('getFacebookFriends', {
             userId: Meteor.userId(),
             all: true
--- client/views/stack/stackFooter.js
+++
@@ -1,8 +0,0 @@
-Template.stackFooter.events({
-    "click .StackRemove": function(event, template) {
-        if (confirm("Are you sure you would like to remove this stack?"))
-            Stack.remove({
-                _id: this._id
-            });
-    }
-});
