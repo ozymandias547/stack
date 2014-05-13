ee48ce3299bb537e2cdc1314bb811d9d1d3a36dc
Joseph Jung <jungyue@gmail.com>
2014-05-12 22:33:16 -0500

    Remove google oauth data

--- server/oauth.js
+++ server/oauth.js
@@ -1,14 +1,6 @@
 Meteor.startup(function() {
 
     Accounts.loginServiceConfiguration.upsert({
-        service: 'google'
-    }, {
-        service: 'google',
-        clientId: '643214640641-mb3urmmre5d06n2k02fjulct70e5a6q9.apps.googleusercontent.com',
-        secret: 'AlMkP_BH4SQ2ju4ItpIKba2c'
-    });
-
-    Accounts.loginServiceConfiguration.upsert({
         service: 'facebook'
     }, {
         service: 'facebook',
